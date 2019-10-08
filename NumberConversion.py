d2fd018991d77339b20620cc85b5b4f0c8be1377
[Gayathri Buddhika] <[wg.gayathri@gmail.com]>
2019-10-08 15:02:44 +0530

    Numeric Conversion
    Numeric Conversion in Python

---
+++ NumberConversion.py
@@ -0,0 +1,19 @@
+# decimal convert to binary, octal and hexadecimal
+a = bin(25)
+print("a in binary format =", a)
+
+b = oct(54)
+print("b in octal format =", b)
+
+c = hex(25)
+print("c in hexadecimal format =", c)
+
+# decimal convert into binary, octal and hexadecimal
+d = 0b11011010
+print("d in decimal format =", d)
+
+e = 0o75
+print("e in decimal format =", e)
+
+f = 0x9af
+print("f in decimal format =", f)
